2ff01fe4753dffe6722a8dc748cde5f82f14b859
Neko <nekodemeo@gmail.com>
2021-02-03 22:47:01 +0900

    Update something

--- test.py
+++ test.py
@@ -2,40 +2,40 @@ import dataset
 import pandas as pd
 
 data = pd.read_csv('TestInput.csv')
-df = pd.DataFrame(data, columns= ['Name','Country','Age'])
+df = pd.DataFrame(data, columns=['Name', 'Country', 'Age'])
 
 print(df)
 
-db = dataset.connect('sqlite:///mydatabase.db')
+db = dataset.connect('sqlite:///testdb.db')
 
 table = db['user']
 for row in df.itertuples():
-   table.insert(dict(Name = row.Name,
-                     Age = row.Age,
-                     Country = row.Country
-                     )
-                )
+    table.insert(dict(Name=row.Name,
+                      Age=row.Age,
+                      Country=row.Country
+                      )
+                 )
 
 users = db['user'].all()
 
 for user in db['user']:
-   print(user)
+    print(user)
 
-#db = dataset.connect('sqlite:///mydatabase.db')
+# db = dataset.connect('sqlite:///mydatabase.db')
 
-#print(db.tables)
-#print(db['user'].columns)
-#print(len(db['user']))
+# print(db.tables)
+# print(db['user'].columns)
+# print(len(db['user']))
 # get a reference to the table 'user'
-#table = db['user']
+# table = db['user']
 
 # Insert a new record.
-#table.insert(dict(name='John Doe', age=46, country='China'))
+# table.insert(dict(name='John Doe', age=46, country='China'))
 
 # dataset will create "missing" columns any time you insert a dict with an unknown key
-#table.insert(dict(name='Jane Doe', age=37, country='France', gender='female'))
+# table.insert(dict(name='Jane Doe', age=37, country='France', gender='female'))
 
-#users = db['user'].all()
+# users = db['user'].all()
 
-#for user in db['user']:
+# for user in db['user']:
 #   print(user)
